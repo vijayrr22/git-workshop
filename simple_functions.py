2975012d98d721d5bc1366d5b844413df3a3d58f
vijayrr22 <162663432+vijayrr22@users.noreply.github.com>
2024-03-15 15:07:02 +0530

    Revert "Added doc string"

--- simple_functions.py
+++ simple_functions.py
@@ -1,9 +1,7 @@
 # Custom python functions
 
 def double_number(a):
-    """double_number gives the double of that given number"""
     return a+a
 
 def square_number(a):
-    """square_number gives the square of that given number"""
     return a*a
