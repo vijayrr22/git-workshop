612eaa5ba0543128a867a34525097b0906498c7f
Harsh Goel <harsh.goel@tigeranalytics.com>
2024-03-15 14:48:18 +0530

    Added doc string

--- simple_functions.py
+++ simple_functions.py
@@ -1,7 +1,9 @@
 # Custom python functions
 
 def double_number(a):
+    """double_number gives the double of that given number"""
     return a+a
 
 def square_number(a):
+    """square_number gives the square of that given number"""
     return a*a
